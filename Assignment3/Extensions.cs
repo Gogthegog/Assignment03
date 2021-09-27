08dfed1d8b4cc4ff14a18e3a2b921455c353c67d
Tessa June Strand <tejs@itu.dk>
2021-09-27 15:20:19 +0200

    worked on Queries

--- Assignment3/Extensions.cs
+++ Assignment3/Extensions.cs
@@ -15,5 +15,16 @@ namespace BDSA2021.Assignment03
             return str.Split(
                 new char[] {' ', '.', '?'}, StringSplitOptions.RemoveEmptyEntries).Length;            
         }
+
+    //     public static void Print<T>(this IEnumerable<T> items)
+    //     {
+    //         foreach (var item in items)
+    //         {
+    //             Console.WriteLine(item);
+    //         }
+    //     }
+
+    //     public static void PrintSingle<T>(this T item) => Console.WriteLine(item);
+    //}
     }
 }
--- Assignment3/Queries.cs
+++ Assignment3/Queries.cs
@@ -4,9 +4,27 @@ namespace BDSA2021.Assignment03
 {
     public class Queries
     {
+        Repository repo = new Repository();
         // public IReadOnlyCollection<Wizard> ByRowling<Wizard>(){
         //     var wizards = Wizard.Wizards.Value.AsEnumerable;
         //     return wizards.Where(w => w.Creator.Contains("Rowling")).Select(w => new {Name = w.Name});
         // }
-    }
+
+
+        /* Exercise 1 - Wizards invented by Rowling. Only return the names.
+        // Using LINQ
+        var ByRowling = from w in Wizard
+                        where w.Name.Contains("Rowling")
+                        select new { Name = w.Name };
+
+        ByRowling.Print();
+
+        // Using Extentions
+        var ByRowlingSorted = Wizard.Where(w => w.Name.Contains("Rowling"))
+                                    .Select(w => new { Name = w.Name });
+        ByRowlingSorted.Print();
+        */
+
+        // Exercise 2 - The year the first sith lord ( Darth something ) was introduced. 
+    }   
 }
